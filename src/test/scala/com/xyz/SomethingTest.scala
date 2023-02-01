469a6f7547fd8f4f3982b3930c20661bb8f5d6ad
Alessandro Candolini <alessandrocandolini@gmail.com>
2023-02-01 23:37:19 +0000

    failing test

--- src/test/scala/com/xyz/SomethingTest.scala
+++ src/test/scala/com/xyz/SomethingTest.scala
@@ -3,12 +3,31 @@ package com.xyz
 import munit.FunSuite
 
 class SomethingTest extends FunSuite:
-  test("something") {
+
+//  test("this test should pass but it fails, uncomment to see it failing") {
+//    assertNoDiff(
+//      compileErrors("Set(2, 1).sorted"),
+//      """|error: value sorted is not a member of scala.collection.immutable.Set[Int]
+//         |Set(2, 1).sorted
+//         |          ^
+//         |""".stripMargin
+//    )
+//  }
+
+  test("this test should fail but it passes") {
     assertNoDiff(
       compileErrors("Set(2, 1).sorted"),
-      """|error: value sorted is not a member of scala.collection.immutable.Set[Int]
+      """|error:
+         |value sorted is not a member of Set[Int], but could be made available as an extension method.
+         |
+         |The following import might make progress towards fixing the problem:
+         |
+         |  import munit.Clue.generate
+         |
          |Set(2, 1).sorted
-         |          ^
+         |         ^
          |""".stripMargin
     )
   }
+
+
